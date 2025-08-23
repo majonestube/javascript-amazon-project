9070240b5b526dc210414724d73ea7595f2f9cbe
majonestube <martine.vold@hotmail.com>
2025-08-23 20:42:49 +0200

    Made cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -71,12 +71,22 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quiantity++;
+        matchingItem.quantity++;
       } else {
         cart.push({
         productId,
-        quiantity: 1
+        quantity: 1
       });
-      };
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
+
