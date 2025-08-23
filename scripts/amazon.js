a68d6dbbe2dc1b803fd3871eb24e3bdf42b446fa
majonestube <martine.vold@hotmail.com>
2025-08-23 20:15:01 +0200

    Used a products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  // better to save money as int, so it's saved in cents
-  price: 1090
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  // better to save money as int, so it's saved in cents
-  price: 2095
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  // better to save money as int, so it's saved in cents
-  price: 799
-}
-];
-
 let productsHTML ='';
 
 products.forEach((product) => {
@@ -54,7 +22,7 @@ products.forEach((product) => {
 
     <div class="product-price">
       <!--Forces two decimals-->
-      $${(product.price / 100).toFixed(2)}
+      $${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
