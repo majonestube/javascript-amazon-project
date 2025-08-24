f87bd1f9cc3fbc802068e8fe19d79bb152d249a9
majonestube <martine.vold@hotmail.com>
2025-08-24 15:03:56 +0200

    Added Promise.All

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,23 +1,59 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-// promise --> run the function immediately
-// resolve is similar to done
-new Promise((resolve) => {
-  loadProducts(() => { 
-    resolve();
-  });
-}).then(() => {
+
+// Like below, but here thet finish all the promises until they do the next step
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => { 
+      resolve('value1');
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadProducts(() => { 
+      resolve('value1');
+    });
+  })
+
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
 
+
 /*
-loadProducts(() => {
+// promise --> run the function immediately
+// resolve is similar to done
+// promises help keeping the code flat, involving a lot of nesting
+new Promise((resolve) => {
+  loadProducts(() => { 
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 });
 */
