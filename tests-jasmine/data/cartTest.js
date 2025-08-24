2c68dd64cf2ca89a4409d6b93a6ef288704b2a8f
majonestube <martine.vold@hotmail.com>
2025-08-24 12:23:37 +0200

    Created automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -7,7 +7,8 @@ describe('addToCart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
       }]);
     });
     loadFromStorage();
