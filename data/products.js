55184c09a17411f1f459bb5a949b9e92a0a6a2bd
majonestube <martine.vold@hotmail.com>
2025-08-24 15:15:39 +0200

    Added loadProductsFetch to some of the code

--- data/products.js
+++ data/products.js
@@ -59,6 +59,27 @@ class Clothing extends Product {
 
 export let products = [];
 
+
+// promise with fetch
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  
+  ).then((response) => {
+    return response.json();
+  
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else {
+        return new Product(productDetails);
+      }});
+  });
+
+  return promise;
+}
+
 // callback
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
