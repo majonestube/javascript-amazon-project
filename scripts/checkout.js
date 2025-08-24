e9687cc7aef8950d4ceaa09237f8bc41e995e032
majonestube <martine.vold@hotmail.com>
2025-08-24 14:47:25 +0200

    Added Promise to checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,20 @@ import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+// promise --> run the function immediately
+// resolve is similar to done
+new Promise((resolve) => {
+  loadProducts(() => { 
+    resolve();
+  });
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
